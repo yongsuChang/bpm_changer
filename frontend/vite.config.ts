7d455f4a6cbbbd922c15c10f26806c9085c70955
YongSu Chang <ahae91@gmail.com>
2026-02-03 13:53:32 +0900

    feat: Add local environment variable searching logics

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: process.env.VITE_ALLOWED_HOST ? [process.env.VITE_ALLOWED_HOST] : []
+  }
 })
